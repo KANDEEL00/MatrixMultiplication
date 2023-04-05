0719f8d0b59cd8be2d2b14dc44fbbda9e596842a
Ahmed Kandeel <1kandeel1@gmail.com>
2023-04-05 23:47:55 +0200

    3alolo effect

--- MatrixMultiplication/MatrixMultiplication.cs
+++ MatrixMultiplication/MatrixMultiplication.cs
@@ -48,7 +48,7 @@
             int[,] h = new int[n, n];
             divideMatrix(M2, ref e, ref f, ref g, ref h, n);
 
-            ////create 4 parts of resresult Matrix M3
+            ////create 4 parts of resresult Matrix M3 (naive)
             //int[,] r = add(MatrixMultiply(a, e, n), MatrixMultiply(b, g, n), n);
             //int[,] s = add(MatrixMultiply(a, f, n), MatrixMultiply(b, h, n), n);
             //int[,] t = add(MatrixMultiply(c, e, n), MatrixMultiply(d, g, n), n);
@@ -81,8 +81,7 @@
             //int[,] P5 = new int[n, n];
             //int[,] P6 = new int[n, n];
             //int[,] P7 = new int[n, n];
-
-            //Parallel.Invoke(() => P1 = MatrixMultiply(a, sub(f, h, n), n),
+            //System.Threading.Tasks.Parallel.Invoke(() => P1 = MatrixMultiply(a, sub(f, h, n), n),
             //                () => P2 = MatrixMultiply(add(a, b, n), h, n),
             //                () => P3 = MatrixMultiply(add(c, d, n), e, n),
             //                () => P4 = MatrixMultiply(d, sub(g, e, n), n),
@@ -103,7 +102,7 @@
             int[,] r = add(add(P5, P6, n), sub(P4, P2, n), n);
             int[,] s = add(P1, P2, n);
             int[,] t = add(P3, P4, n);
-            int[,] u = sub(sub(add(P5, P1, n), P3, n), P7, n);
+            int[,] u = sub(add(P5, P1, n), add(P3, P7, n), n);
 
             //assemble result Matrix M3
             assembleMatrix(ref M3, r, s, t, u, n);
@@ -130,42 +129,26 @@
 
         static public void divideMatrix(int[,] M, ref int[,] a, ref int[,] b, ref int[,] c, ref int[,] d, int n)
         {
-            //11 quarter
             for (int i = 0; i < n; i++)
                 for (int j = 0; j < n; j++)
+                {
                     a[i, j] = M[i, j];
-            //12 quarter
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
                     b[i, j] = M[i, j + n];
-            //21 quarter
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
                     c[i, j] = M[i + n, j];
-            //22 quarter
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
                     d[i, j] = M[i + n, j + n];
+                }
         }
 
         static public void assembleMatrix(ref int[,] M, int[,] r, int[,] s, int[,] t, int[,] u, int n)
         {
-            //11 quarter
             for (int i = 0; i < n; i++)
                 for (int j = 0; j < n; j++)
+                {
                     M[i, j] = r[i, j];
-            //12 quarter
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
                     M[i, j + n] = s[i, j];
-            //21 quarter
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
                     M[i + n, j] = t[i, j];
-            //22 quarter
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
                     M[i + n, j + n] = u[i, j];
+                }
         }
 
         #endregion
